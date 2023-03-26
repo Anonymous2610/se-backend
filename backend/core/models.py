5385224692ee86973a6f7225680cbba9d328344a
Chandan Kumar Haboda <78662053+Anonymous2610@users.noreply.github.com>
2023-03-26 22:44:06 +0530

    Update models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -25,7 +25,7 @@ class HouseKeeping(models.Model):
     status = models.CharField(choices=[('clean', 'Clean'), ('dirty', 'Dirty')], max_length=10, default='dirty')
     inventory = models.CharField(choices=[('available', 'Available'), ('not available', 'Not Available')], max_length=20, default='not available')
     def __str__(self):
-        return self.status
+        return "Room No. "+self.room_no
 
 class Analytics(models.Model):
     choices=[('january', 'January'), ('february', 'February'), ('march', 'March'), ('april', 'April'), ('may', 'May'), ('june', 'June'), ('july', 'July'), ('august', 'August'), ('september', 'September'), ('october', 'October'), ('november', 'November'), ('december', 'December')]
